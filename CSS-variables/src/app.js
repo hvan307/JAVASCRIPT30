5238966e0ff05b001e2201c505b397bcfd23615a
hvan307 <hongvan307@gmail.com>
2020-05-26 17:47:35 +0100

    complete

---
+++ CSS-variables/src/app.js
@@ -0,0 +1,14 @@
+function main() {
+  const inputs = document.querySelectorAll('.controls input')
+
+  function handleUpdate() {
+    // console.log(this.dataset)
+    const suffix = this.dataset.sizing || ''
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix)
+  }
+  
+  inputs.forEach(input => input.addEventListener('change', handleUpdate))
+  inputs.forEach(input => input.addEventListener('mousemove', handleUpdate))
+}
+
+window.addEventListener('DOMContentLoaded', main)
