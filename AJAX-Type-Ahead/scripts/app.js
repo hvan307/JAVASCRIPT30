f2797ee804938ffa1ba96761b9da399e28e2bc8f
hvan307 <hongvan307@gmail.com>
2020-05-29 21:19:55 +0100

    css and some js"

--- AJAX-Type-Ahead/scripts/app.js
+++ AJAX-Type-Ahead/scripts/app.js
@@ -1,8 +1,31 @@
 function main() {
 
-  fetch('https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json')
+  const searchedWord = document.querySelector('.search')
+  const suggestions = document.querySelector('.suggestions')
 
+  const cities = []
+
+  fetch('https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json')
+    .then(resp => resp.json())
+    .then(data => cities.push(...data))
+
+  function findMatches(wordToMatch, cities) {
+    return cities.filter(place => {
+      const regex = new RegExp(wordToMatch, 'gi') // g for Global, i (case)Insensitive
+      return place.city.match(regex) || place.state.match(regex)
+    })
+  }
+  function displayMatches() {
+    const matches = findMatches(this.value, cities)
+    console.log(matches)
+  }
+
+  searchedWord.addEventListener('change', displayMatches)
+  searchedWord.addEventListener('keyup', displayMatches)
+
+  suggestions.addEventListener('change', displayMatches)
 
 }
+
 
 window.addEventListener('DOMContentLoaded', main)
