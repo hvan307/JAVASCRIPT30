52714d69ef8cbc660547da38353cf5e6a3c48a9b
hvan307 <hongvan307@gmail.com>
2020-05-30 23:08:18 +0100

    complete

--- Array-Cardio2/scripts/app.js
+++ Array-Cardio2/scripts/app.js
@@ -4,7 +4,7 @@ function main() {
     { name: 'Kait', year: 1986 },
     { name: 'Irv', year: 1970 },
     { name: 'Lux', year: 2015 }
-  ];
+  ]
 
   const comments = [
     { text: 'Love this!', id: 523423 },
@@ -12,19 +12,32 @@ function main() {
     { text: 'You are the best', id: 2039842 },
     { text: 'Ramen is my fav food ever', id: 123523 },
     { text: 'Nice Nice Nice!', id: 542328 }
-  ];
+  ]
 
   // Some and Every Checks
   // Array.prototype.some() // is at least one person 19 or older?
+  const isOver19 = people.some(person => {
+    const currentYear = new Date().getFullYear()
+    return currentYear - person.year >= 19
+  })
+  console.log(isOver19)
+
   // Array.prototype.every() // is everyone 19 or older?
+  const AllOver19 = people.every(person => (new Date().getFullYear()) - person.year >= 19)
+  console.log(AllOver19)
 
   // Array.prototype.find()
   // Find is like filter, but instead returns just the one you are looking for
   // find the comment with the ID of 823423
+  const comment = comments.find(comment => comment.id === 823423)
+  console.log(comment)
 
   // Array.prototype.findIndex()
   // Find the comment with this ID
   // delete the comment with the ID of 823423
+  const index = comments.findIndex(comment => comment.id === 823423)
+  comments.splice(index, 1)
+  console.log(comments)
 }
 
 window.addEventListener('DOMContentLoaded', main)
