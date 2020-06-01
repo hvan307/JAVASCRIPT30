b321bed5d49af17872845f7d8f3ed7f887c99dfc
hvan307 <hongvan307@gmail.com>
2020-06-01 19:04:09 +0100

    completed

--- Hold-Shift-Multiple-Checkboxes/scripts/app.js
+++ Hold-Shift-Multiple-Checkboxes/scripts/app.js
@@ -1,5 +1,27 @@
 function main() {
+  const checkboxes = document.querySelectorAll('input')
 
+  let lastChecked
+
+  function handleCheck(e) {
+    let inBetween = false
+    if (e.shiftKey && this.checked) {
+      checkboxes.forEach(checkbox => {
+        // console.log(checkbox)
+        if (checkbox === this || checkbox === lastChecked) {
+          inBetween = !inBetween
+        }
+        if (inBetween) {
+          checkbox.checked = true
+        }
+      })
+    }
+    lastChecked = this // the last box we checked
+  }
+
+  checkboxes.forEach(checkbox => {
+    checkbox.addEventListener('click', handleCheck)
+  })
 }
 
 window.addEventListener('DOMContentLoaded', main)
