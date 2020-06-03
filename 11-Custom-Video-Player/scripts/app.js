fe32f6db07893fdd7b80233fca580cb629fc2240
hvan307 <hongvan307@gmail.com>
2020-06-03 19:53:21 +0100

    imports

---
+++ 11-Custom-Video-Player/scripts/app.js
@@ -0,0 +1,14 @@
+function main() {
+// imports
+const player = document.querySelector('.player')
+const video = player.querySelector('.viewer')
+const progress = player.querySelector('.progress')
+const progressBar = player.querySelector('.progress__filed')
+const toggle = player.querySelector('.toggle')
+const skipButtons = player.querySelectorAll('[data-skip]')
+const ranges = player.querySelectorAll('.player__slider')
+
+
+}
+
+window.addEventListener('DOMContentLoaded', main)
