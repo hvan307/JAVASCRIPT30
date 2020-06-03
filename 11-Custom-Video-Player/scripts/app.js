39ffc86756d5092d0cadd8e13d19af14a05e3648
hvan307 <hongvan307@gmail.com>
2020-06-03 20:01:34 +0100

    play/pause button working

--- 11-Custom-Video-Player/scripts/app.js
+++ 11-Custom-Video-Player/scripts/app.js
@@ -1,14 +1,26 @@
 function main() {
-// imports
-const player = document.querySelector('.player')
-const video = player.querySelector('.viewer')
-const progress = player.querySelector('.progress')
-const progressBar = player.querySelector('.progress__filed')
-const toggle = player.querySelector('.toggle')
-const skipButtons = player.querySelectorAll('[data-skip]')
-const ranges = player.querySelectorAll('.player__slider')
+  // imports
+  const player = document.querySelector('.player')
+  const video = player.querySelector('.viewer')
+  const progress = player.querySelector('.progress')
+  const progressBar = player.querySelector('.progress__filed')
+  const toggle = player.querySelector('.toggle')
+  const skipButtons = player.querySelectorAll('[data-skip]')
+  const ranges = player.querySelectorAll('.player__slider')
 
+  function togglePlay() {
+    // use video.paused property -> there's no play property
+    if (video.paused) {
+      console.log('hello')
+      video.play()
+      toggle.innerHTML = '▶️'
+    } else {
+      video.pause()
+      toggle.innerHTML = '⏸'
+    }
+  }
 
+  toggle.addEventListener('click', togglePlay)
 }
 
 window.addEventListener('DOMContentLoaded', main)
