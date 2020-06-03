486075e9b5c7b4d6bc6f1cc783ceee740569a2a9
hvan307 <hongvan307@gmail.com>
2020-06-03 23:56:23 +0100

    completed

--- 11-Custom-Video-Player/scripts/app.js
+++ 11-Custom-Video-Player/scripts/app.js
@@ -3,24 +3,59 @@ function main() {
   const player = document.querySelector('.player')
   const video = player.querySelector('.viewer')
   const progress = player.querySelector('.progress')
-  const progressBar = player.querySelector('.progress__filed')
+  const progressBar = player.querySelector('.progress__filled')
   const toggle = player.querySelector('.toggle')
   const skipButtons = player.querySelectorAll('[data-skip]')
   const ranges = player.querySelectorAll('.player__slider')
 
   function togglePlay() {
     // use video.paused property -> there's no play property
-    if (video.paused) {
-      console.log('hello')
-      video.play()
-      toggle.innerHTML = '▶️'
-    } else {
-      video.pause()
-      toggle.innerHTML = '⏸'
-    }
+    const property = video.paused ? 'play' : 'pause'
+    video[property]()
   }
 
+  function updateButton() {
+    toggle.innerHTML = this.paused ? '▶️' : '⏸'
+  }
+
+  function skip() {
+    // console.log(this.dataset)
+    video.currentTime += parseFloat(this.dataset.skip)
+  }
+
+  function handleRangeUpdate() {
+    // console.log(this.value)
+    // console.log(this.name)
+    video[this.name] = this.value
+  }
+
+  function handleProgress() {
+    // We need to calculate the progress in % as suggested by flex-basis in main.css
+    const percent = (video.currentTime / video.duration) * 100
+    progressBar.style.flexBasis = `${percent}%` 
+  }
+
+  function scrub(e) {
+    // console.log(e)
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration
+    // console.log(scrubTime)
+    video.currentTime = scrubTime
+  }
+
+  video.addEventListener('click', togglePlay)
+  video.addEventListener('play', updateButton)
+  video.addEventListener('pause', updateButton)
+  video.addEventListener('timeupdate', handleProgress)
   toggle.addEventListener('click', togglePlay)
+  skipButtons.forEach(skipButton => skipButton.addEventListener('click', skip))
+  ranges.forEach(range => range.addEventListener('change', handleRangeUpdate))
+  ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate))
+
+  let mousedown = false
+  progress.addEventListener('click', scrub)
+  progress.addEventListener('mousemove', (e) => mousedown && scrub(e))
+  progress.addEventListener('mousedown', () => mousedown = true)
+  progress.addEventListener('mouseup', () => mousedown = false)
 }
 
 window.addEventListener('DOMContentLoaded', main)
